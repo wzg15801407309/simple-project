69a264ccba5a205f1cd3ce2443c8be130129c85f
wzg15801407309 <1051084301@qq.com>
2020-04-18 11:12:29 +0800

    格式文件的添加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
-};
+    root: true,
+    env: {
+      node: true
+    },
+    'extends': [
+      'plugin:vue/essential',
+      'eslint:recommended'
+     ],
+    parserOptions: {
+      parser: 'babel-eslint'
+    },
+    rules: {
+     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //强制使用单引号
+    quotes: ['error', 'single'],
+    //在结尾处，强制不使用分号结尾
+    semi: ['error', 'never']
+   }
+}
